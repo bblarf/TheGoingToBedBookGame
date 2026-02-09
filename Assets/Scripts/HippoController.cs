fdbdd4b28765d2aebbf6983ed1519c7257b63b65
bblarf <143335865+bblarf@users.noreply.github.com>
2026-02-09 13:08:02 -0500

    Initial commit

---
+++ Assets/Scripts/HippoController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class HippoController : MonoBehaviour
+{
+    public float speed = .1f;
+    public float jumpHeight = .5f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Vector2 curPos = gameObject.transform.position;
+            Vector2 newPos = new Vector2(curPos.x + Time.deltaTime * speed, curPos.y);
+            gameObject.transform.position = newPos;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector2 curPos = gameObject.transform.position;
+            Vector2 newPos = new Vector2(curPos.x - Time.deltaTime * speed, curPos.y);
+            gameObject.transform.position = newPos;
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Vector2 curPos = gameObject.transform.position;
+            Vector2 newPos = new Vector2(curPos.x, curPos.y + jumpHeight);
+            gameObject.transform.position = newPos;
+        }
+    }
+}
